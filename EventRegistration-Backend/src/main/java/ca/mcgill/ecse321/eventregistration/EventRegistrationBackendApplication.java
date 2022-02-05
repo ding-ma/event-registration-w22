5cf3d668075e136d59927333a61b960fcc84ca4b
Ding Ma <ding.ma@mail.mcgill.ca>
2022-02-05 11:34:17 -0500

    backend setup

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplication.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EventRegistrationBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(EventRegistrationBackendApplication.class, args);
+    }
+    
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello world!";
+    }
+}
